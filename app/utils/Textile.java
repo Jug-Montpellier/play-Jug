708b95a61682fca9c9017c01151628b8fc5a320d
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-06-22 18:14:47 +0200

    Fix bad html parsing

--- app/utils/Textile.java
+++ app/utils/Textile.java
@@ -7,7 +7,11 @@ public class Textile {
 
     public static String toHTML(String textile) {
         String html = new MarkupParser(new TextileLanguage()).parseToHtml(textile);
-        html = html.substring(html.indexOf("<body>") + 6, html.lastIndexOf("</body>"));
+        if (html.contains("<body><p>") && html.contains("</p></body>")) {
+        	html = html.substring(html.indexOf("<body><p>") + 9, html.lastIndexOf("</p></body>"));
+        } else {
+        	html = html.substring(html.indexOf("<body>") + 6, html.lastIndexOf("</body>"));        	
+        }
         return html;
     }
 
